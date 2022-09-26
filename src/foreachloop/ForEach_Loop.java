019bdc14977e2a632c2996d5c7734ebcb8e4379d
meghamapala@gmail.com <meghamapala@gmail.com>
2022-09-26 09:23:51 -0400

    first commit

---
+++ src/foreachloop/ForEach_Loop.java
@@ -0,0 +1,27 @@
+package foreachloop;
+
+public class ForEach_Loop {
+
+	public static void main(String[] args) {
+		
+		int a [] = {4,6,5,9,8};
+
+		for (int x: a)
+
+		{
+			
+			System.out.println(x);
+
+
+		}
+		
+		String cars [] = {"BMW","Toyota","Benz","Cheavy","Audi"};
+		
+		for(String c: cars)
+			
+		{
+			System.out.println(c);
+		}
+	}
+
+}
